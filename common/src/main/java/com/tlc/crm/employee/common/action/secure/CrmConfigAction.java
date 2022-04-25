da00caa6891f208c847fe336bd83c0e226e43947
DevidurgaA <102131763+DevidurgaA@users.noreply.github.com>
2022-04-25 18:54:55 +0530

    api implementation

---
+++ common/src/main/java/com/tlc/crm/employee/common/action/secure/CrmConfigAction.java
@@ -0,0 +1,101 @@
+package com.tlc.crm.employee.common.action.secure;
+
+import com.tlc.commons.json.Json;
+import com.tlc.commons.json.JsonArray;
+import com.tlc.commons.json.JsonObject;
+import com.tlc.crm.employee.common.config.ConfigManager;
+import com.tlc.crm.employee.common.action.CrmRequest;
+import com.tlc.validator.TlcModel;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * @author Abishek
+ * @version 1.0
+ */
+public abstract class CrmConfigAction<T extends TlcModel> extends CrmSecureAction
+{
+    @Override
+    public final JsonObject sProcess(CrmRequest request)
+    {
+        final JsonObject responseJson = Json.object();
+        final JsonObject requestJson = request.getRequestJson();
+
+        final String type = requestJson.getString("type");
+        final JsonObject data = requestJson.getJsonObject("data");
+
+        data.put("type", type);
+
+        switch (type)
+        {
+            case "create" ->
+                    {
+                        Collection<T> model = constructArray(data);
+                        create(model);
+                    }
+            case "update" ->
+                    {
+                        final T model = construct(data);
+                        update(model);
+                    }
+            case "delete" ->
+                    {
+                        final Long id = data.getLong("id");
+                        final T model = partialGet(id);
+                        delete(model);
+                    }
+            case "exists" ->
+                    {
+                        final T model = construct(data);
+                        final boolean exists = exists(model);
+                        responseJson.put("found", exists);
+                    }
+            default ->
+                    {
+                        final Long id = data.getLong("id");
+                        final T model = get(id);
+                        responseJson.put("id", construct(model));
+                    }
+        }
+        return responseJson;
+    }
+
+    public abstract JsonObject construct(T model);
+
+    public void create(Collection<T> model)
+    {
+        getConfigManager().create(model);
+    }
+
+    public void update(T model)
+    {
+        getConfigManager().update(model);
+    }
+
+    public void delete(T model)
+    {
+        getConfigManager().delete(model);
+    }
+
+    public boolean exists(T model)
+    {
+        return getConfigManager().exists(model);
+    }
+
+    public T partialGet(Long id)
+    {
+        return getConfigManager().partialGet(id);
+    }
+
+    public T get(Long id)
+    {
+        return getConfigManager().get(id);
+    }
+
+    public abstract ConfigManager<T> getConfigManager();
+
+    public abstract T construct(JsonObject jsonObject);
+
+    public abstract Collection<T> constructArray(JsonObject jsonObject);
+}
---
+++ common/src/main/java/com/tlc/crm/employee/common/config/AuditCategory.java
@@ -0,0 +1,9 @@
+package com.tlc.crm.employee.common.config;
+
+/**
+ * @author Abishek
+ * @version 1.0
+ */
+public record AuditCategory(String name)
+{
+}
